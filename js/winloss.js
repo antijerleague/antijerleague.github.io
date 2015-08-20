bd2a093053e311cc6d0d123e11302fadec005c94
Mike Horn <mhorn918@gmail.com>
2015-08-20 08:16:39 -0400

    winloss page

---
+++ js/winloss.js
@@ -0,0 +1,33 @@
+jQuery(function() {
+  if(window.location.pathname =="/winloss.html"){ 
+
+function grow(){
+  var element = '.bar';
+  var i = 1;
+
+  $(element).each(function(index,value){
+    var percent = $(this).attr('data-percent');
+    var percentBarDisplay = $(this).attr('data-percent-bar-display');
+    var name = $(this).attr('data-name');
+    var record = $(this).attr('data-record');
+    var timing = percent / 150;
+    setTimeout(function(){
+
+      $(value).css('max-width',+ percentBarDisplay + '%').css('transition', timing + 's ease all');
+      $(value).append('<div class="num">'+'<strong>'+name+'</strong>'+" "+record+'</div>');
+
+    }, i * 50);
+    
+    i++;
+  });
+      }
+
+      
+
+      window.onload = function(){
+        
+  requestAnimationFrame(grow);
+
+      };
+  }
+})
