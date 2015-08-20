bc78170e193319ae4009e0502a848dfa2eeb414a
Mike Horn <mhorn918@gmail.com>
2015-08-20 07:51:23 -0400

    update percent bar length

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-
  function grow(){
   var element = '.bar';
   var i = 1;
 
   $(element).each(function(index,value){
     var percent = $(this).attr('data-percent');
+    var percentBarDisplay = $(this).attr('data-percent-bar-display');
     var name = $(this).attr('data-name');
     var record = $(this).attr('data-record');
     var timing = percent / 150;
     setTimeout(function(){
 
-      $(value).css('max-width',+ percent + '%').css('transition', timing + 's ease all');
+      $(value).css('max-width',+ percentBarDisplay + '%').css('transition', timing + 's ease all');
       $(value).append('<div class="num">'+'<strong>'+name+'</strong>'+" "+percent+'%</div>');
 
     }, i * 50);
