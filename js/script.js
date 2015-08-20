6b231e82badbbbc23379b573574799e3a9ac8b53
Mike Horn <mhorn918@gmail.com>
2015-08-19 23:38:27 -0400

    updates to html, css, and js for all-time winning %

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+ function grow(){
+  var element = '.bar';
+  var i = 1;
+
+  $(element).each(function(index,value){
+    var percent = $(this).attr('data-percent');
+    var name = $(this).attr('data-name');
+    var record = $(this).attr('data-record');
+    var timing = percent / 150;
+    setTimeout(function(){
+
+      $(value).css('max-width',+ percent + '%').css('transition', timing + 's ease all');
+      $(value).append('<div class="num">'+'<strong>'+name+'</strong>'+" "+percent+'%</div>');
+
+    }, i * 50);
+    
+    i++;
+  });
+      }
+
+      
+
+      window.onload = function(){
+        
+  requestAnimationFrame(grow);
+
+      };
